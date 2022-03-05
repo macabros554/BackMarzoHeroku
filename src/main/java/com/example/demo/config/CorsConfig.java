e9306942d20555aaa55559575d883a9a03970079
macabros554 <macabros554@gmail.com>
2022-03-05 13:41:53 +0100

    ruta de acceso cambiada

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,92 +12,93 @@ public class CorsConfig implements WebMvcConfigurer {
 	public WebMvcConfigurer corsConfigurer() {return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
+				String ruta="https://macabros554.github.io/FrontMarzo2/";
 				//login
-				registry.addMapping("/auth/login").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/auth/login").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//register
-				registry.addMapping("/auth/register").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/auth/register").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//consultarToken
-				registry.addMapping("/validarToken").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/validarToken").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//listaDeOrdenadores
-				registry.addMapping("/ordenador/listaOrdenadores").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/ordenador/listaOrdenadores").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//añadir Ordenador
-				registry.addMapping("/ordenador").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/ordenador").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//ordenador
-				registry.addMapping("/ordenador/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/ordenador/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//procesadoresCompatibles
-				registry.addMapping("/componente/procesador/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/componente/procesador/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//ramsCompatibles
-				registry.addMapping("/componente/ram/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/componente/ram/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//DiscosDuros
-				registry.addMapping("/componente/discos/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/componente/discos/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//Graficas
-				registry.addMapping("/componente/graficas/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/componente/graficas/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//Fuentes
-				registry.addMapping("/componente/fuentes/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/componente/fuentes/{id}").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//Crear pedido
-				registry.addMapping("/pedido").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/pedido").allowedOrigins(ruta)
 				.allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "DELETE", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//leerPedido BorrarPedido
-				registry.addMapping("/pedido/{id}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/pedido/{id}").allowedOrigins(ruta)
 				.allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				
-				registry.addMapping("pedido/{idPedido}/ordenadornuevo/{idOrdenador}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("pedido/{idPedido}/ordenadornuevo/{idOrdenador}").allowedOrigins(ruta)
 				.allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				
-				registry.addMapping("/pedido/{id}/ordenadornuevo").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/pedido/{id}/ordenadornuevo").allowedOrigins(ruta)
 				.allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//usuario
-				registry.addMapping("/usuario").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/usuario").allowedOrigins(ruta)
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
 		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 				//borrar usuario
-				registry.addMapping("/usuario/{email}").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/usuario/{email}").allowedOrigins(ruta)
 				.allowedMethods("GET", "POST", "OPTIONS", "PUT","DELETE")
 		        .allowedHeaders("GET", "POST", "OPTIONS", "PUT","DELETE", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
 		                "Access-Control-Request-Headers","Authorization")
