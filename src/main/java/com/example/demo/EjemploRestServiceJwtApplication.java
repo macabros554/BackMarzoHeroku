c046f5539e8492aca34fadf0e4c0a838e55be29d
macabros554 <macabros554@gmail.com>
2022-03-06 15:49:52 +0100

    posibilidad de crear war creada

--- src/main/java/com/example/demo/EjemploRestServiceJwtApplication.java
+++ src/main/java/com/example/demo/EjemploRestServiceJwtApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -30,7 +32,11 @@ import com.example.demo.service.ProcesadorService;
 import com.example.demo.service.RamService;
 
 @SpringBootApplication
-public class EjemploRestServiceJwtApplication {
+public class EjemploRestServiceJwtApplication extends SpringBootServletInitializer{
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EjemploRestServiceJwtApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjemploRestServiceJwtApplication.class, args);
